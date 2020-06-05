3262335b3476fc53deff13d2f163d10d8f8befa5
abakirh <abakirhanna@gmail.com>
2020-06-05 07:40:31 -0400

    added an id variable to make each contact have a unique trait

--- portfolio/src/main/java/com/google/sps/data/Contact.java
+++ portfolio/src/main/java/com/google/sps/data/Contact.java
@@ -6,10 +6,12 @@ public final class Contact {
     private final String name;
     private final String email;
     private final long number;
+    private final long id;
   
-    public Contact(String name, String email, long number) {
+    public Contact(String name, String email, long number, long id) {
       this.name = name;
       this.email = email;
       this.number = number;
+      this.id = id;
     }
   }
