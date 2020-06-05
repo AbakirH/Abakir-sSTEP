78a95f77cb3d7fde26702e0ed87e8e3b24fe88b5
abakirh <abakirhanna@gmail.com>
2020-06-05 00:54:07 -0400

    created a contact java class to be used as a structure format

---
+++ portfolio/src/main/java/com/google/sps/data/Contact.java
@@ -0,0 +1,15 @@
+
+package com.google.sps.data;
+
+public final class Contact {
+
+    private final String name;
+    private final String email;
+    private final long number;
+  
+    public Contact(String name, String email, long number) {
+      this.name = name;
+      this.email = email;
+      this.number = number;
+    }
+  }
