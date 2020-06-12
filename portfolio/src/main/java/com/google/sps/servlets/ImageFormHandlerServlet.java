62945a87ca36b3560fad4e2475bb90dbf0df28a5
abakirh <abakirhanna@gmail.com>
2020-06-12 08:04:00 -0400

    fixed comment styling for a function

--- portfolio/src/main/java/com/google/sps/servlets/ImageFormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ImageFormHandlerServlet.java
@@ -68,7 +68,7 @@ public class ImageFormHandlerServlet extends HttpServlet {
     response.sendRedirect("/index.html");
   }
 
-  /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
+  // Returns a URL that points to the uploaded file, or null if the user didn't upload a file.
   private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
